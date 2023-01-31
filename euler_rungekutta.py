e2ba5278a77ee5c4c13690cf8ca71ebe9f9bed8e
ifcassianasl <cassianasilveira64@gmail.com>
2023-01-31 17:28:41 -0300

    Add files via upload

--- euler_rungekutta.py
+++ euler_rungekutta.py
@@ -6,7 +6,7 @@ runge_kutta_y = []
 
 tempo = 0
 passo = 1e-2
-n = 50
+n = 15
 y0 = 300
 
 def func (t, y0): 
@@ -16,10 +16,14 @@ def func (t, y0):
 def euler_function (func, y0, tempo, passo, n) :
   euler_y.append(y0)
   for i in range(n):
-    yk = y0 + passo * func(tempo, y0)
-    euler_y.append(yk)
-    y0 = yk
+    yk = euler_y[i]
+    yk1 = yk + passo * func(tempo, yk)
+    euler_y.append(yk1)
     tempo = tempo + passo
+
+    if yk1 <= y0/2:
+      return tempo
+
 
 
 def runge_kutta_function (func, y0, h, n):
@@ -27,6 +31,8 @@ def runge_kutta_function (func, y0, h, n):
 
   for i in range(n):
     yk = runge_kutta_y[i]
+    if yk <= y0/2:
+      return t
 
     t = (i-1)*h
     k1 = func(t, yk)
@@ -37,16 +43,22 @@ def runge_kutta_function (func, y0, h, n):
     runge_kutta_y.append(yk + (h/6)*(k1 + (2*k2) + (2*k3)+ k4))
 
 
-euler_function(func, y0, tempo, passo, n)
-runge_kutta_function(func, y0, passo, n)
+
+euler_tempo = euler_function(func, y0, tempo, passo, n)
+runge_kutta_tempo = runge_kutta_function(func, y0, passo, n)
+
+print(euler_tempo)
+print(runge_kutta_tempo)
+
+print ("{:<15}".format('Euler'))
+for i in euler_y: 
+  print ("{:<15.7f}".format(i))
+
+print ("{:<15}".format('Runge-Kutta'))
+for i in runge_kutta_y: 
+  print ("{:<15.7f}".format(i))
 
 
-print ("{:<15} {:<15}".format('Euler','Range-Kutta'))
-
-for i in range(n): 
-  print ("{:<15.7f} {:<15.7f}".format(euler_y[i], runge_kutta_y[i]))
-
-
-plt.plot(runge_kutta_y)    
-plt.plot(euler_y)    
+plt.plot(runge_kutta_y, color='red')    
+plt.plot(euler_y, color='green')    
 plt.show()
