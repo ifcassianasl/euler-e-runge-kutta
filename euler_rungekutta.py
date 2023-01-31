0626a7602e74657cd0313dcd81e97431f07e25e9
ifcassianasl <cassianasilveira64@gmail.com>
2023-01-31 18:16:18 -0300

    Add files via upload

--- euler_rungekutta.py
+++ euler_rungekutta.py
@@ -1,25 +1,29 @@
 
 import matplotlib.pyplot as plt 
+import numpy as np
 
 euler_y = []
 runge_kutta_y = []
+tempo_utilizado = [0]
 
 tempo = 0
 passo = 1e-2
-n = 15
+n = 1
 y0 = 300
 
 def func (t, y0): 
   return -10*y0
 
 
-def euler_function (func, y0, tempo, passo, n) :
+def euler_function (func, y0, tempo, passo, n):
+  itmax = int(n/passo)
   euler_y.append(y0)
-  for i in range(n):
+  for i in range(itmax):
     yk = euler_y[i]
     yk1 = yk + passo * func(tempo, yk)
     euler_y.append(yk1)
     tempo = tempo + passo
+    tempo_utilizado.append(tempo)
 
     if yk1 <= y0/2:
       return tempo
@@ -28,8 +32,9 @@ def euler_function (func, y0, tempo, passo, n) :
 
 def runge_kutta_function (func, y0, h, n):
   runge_kutta_y.append(y0)
+  itmax = int(n/h)
 
-  for i in range(n):
+  for i in range(itmax):
     yk = runge_kutta_y[i]
     if yk <= y0/2:
       return t
@@ -47,18 +52,29 @@ def runge_kutta_function (func, y0, h, n):
 euler_tempo = euler_function(func, y0, tempo, passo, n)
 runge_kutta_tempo = runge_kutta_function(func, y0, passo, n)
 
-print(euler_tempo)
-print(runge_kutta_tempo)
+print("Tempo em Euler", euler_tempo)
 
 print ("{:<15}".format('Euler'))
 for i in euler_y: 
   print ("{:<15.7f}".format(i))
 
+print("\n\n\n\nTempo em Runge-Kutta", runge_kutta_tempo)
 print ("{:<15}".format('Runge-Kutta'))
 for i in runge_kutta_y: 
   print ("{:<15.7f}".format(i))
 
+def func_teorico (t):
+  return 300*np.exp(-10*t)
 
-plt.plot(runge_kutta_y, color='red')    
-plt.plot(euler_y, color='green')    
+print ("{:<15}".format('Resultados analíticos'))
+resultados_teoricos = []
+for i in tempo_utilizado:
+  resultado = func_teorico(i)
+  resultados_teoricos.append(resultado)
+  print ("{:<15.7f}".format(resultado))
+
+
+plt.plot(runge_kutta_y, color='green')    
+plt.plot(euler_y, color='red')    
+plt.plot(resultados_teoricos, color='black', linestyle="dotted")    
 plt.show()
